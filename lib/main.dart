300f7c3249ac4478d9a732b6dfb9f4e2d04f20b6
Paul Babu <33522991+paulbabu1999@users.noreply.github.com>
2023-09-19 08:18:48 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // ClusterManager.createMarkers();
+    //  ClusterManager.createMarkers();
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
